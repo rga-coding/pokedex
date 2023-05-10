57a74a13b7c641e5f1b550a1fc5e1e9702263dbf
Gurkan Akcakaya <gurkan.akcakaya@cegeka.com>
2023-05-10 17:38:12 +0200

    [FEAT] - logic to add pokemon from json file

--- pokedex/forms.py
+++ pokedex/forms.py
@@ -1,12 +1,22 @@
+import json
+
 from django import forms
 
 from pokedex.models import Pokemon, PokemonFile
+from pokedex.utils import add_pokeapi_pokemon
+
+
 class PokemonFileForm(forms.ModelForm):
     pokemon_json_file = forms.FileField(required=False)
 
     class Meta:
         model = PokemonFile
         fields = ('name', )
+
+    def save(self, commit=True):
+        add_pokeapi_pokemon(json.load(self.cleaned_data['pokemon_json_file']))
+        return super().save(commit=commit)
+
 
 class PokemonForm(forms.ModelForm):
 
